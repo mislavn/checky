1263c10c7964ff18ba6c983d890b9e6f6a40249b
Mislav Novakovic <mislav.novakovic@gmail.com>
2023-09-24 22:24:22 +0300

    Add Leaf types
    Signed-off-by: Mislav Novakovic <mislav.novakovic@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,22 @@ pub struct Leaf {
     description: String,
 }
 
+pub enum LeafType {
+    String(StringType),
+    Number(NumberType),
+}
+
+pub struct StringType {
+    pub pattern: Option<String>,
+    pub min_length: Option<usize>,
+    pub max_length: Option<usize>,
+}
+
+pub struct NumberType {
+    pub min: Option<usize>,
+    pub max: Option<usize>,
+}
+
 impl fmt::Debug for Leaf {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Leaf {}: {}", self.name, self.description)
