bacd5d44f210e0cbf7845851ea33493f65b40ceb
Mislav Novakovic <mislav.novakovic@gmail.com>
2023-09-25 23:42:52 +0300

    Add serde
    Signed-off-by: Mislav Novakovic <mislav.novakovic@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,33 @@
+use serde::{Deserialize, Serialize};
 use std::fmt;
 
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Leaf {
     name: String,
     description: String,
 }
 
+#[derive(Clone, Serialize, Deserialize)]
 pub enum LeafType<T> {
     String(StringType),
     Number(NumberType<T>),
     List(ListType<T>),
 }
 
+#[derive(Clone, Serialize, Deserialize)]
 pub struct StringType {
     pub pattern: Option<String>,
     pub min_length: Option<usize>,
     pub max_length: Option<usize>,
 }
 
+#[derive(Clone, Serialize, Deserialize)]
 pub struct NumberType<T> {
     pub min: Option<T>,
     pub max: Option<T>,
 }
 
+#[derive(Clone, Serialize, Deserialize)]
 pub struct ListType<T> {
     pub min: Option<T>,
     pub max: Option<T>,
